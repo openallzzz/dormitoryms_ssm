1a22eb23e7cb17a34265f946a52ae0b99594c37b
zhangyu <1802833394@qq.com>
2023-01-20 15:42:33 +0800

    添加安全退出功能

--- src/main/java/org/zzzzzz/controller/AccountController.java
+++ src/main/java/org/zzzzzz/controller/AccountController.java
@@ -2,6 +2,7 @@ package org.zzzzzz.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -37,6 +38,12 @@ public class AccountController {
                 break;
         }
         return modelAndView;
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "login";
     }
 
 }
